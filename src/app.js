cd16a9c4627e36ccd733cf0a2ddd640b03f33ebb
student <student@localhost>
2019-07-12 21:38:56 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import {Liker, Mem} from "./lib";
+import {Liker} from "./lib";
 
 const server = express();
 server.use(cors());
